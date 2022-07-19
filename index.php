d5844a430e6c2986f0bd5409ab0dd390d6ca4441
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-07-19 09:07:41 +0100

    Creating Newsfeed posts and correct loading with ajax

--- index.php
+++ index.php
@@ -34,13 +34,61 @@ if(isset($_POST['post'])) {
             <hr />
         </form>
 
-        <?php
+        
+        <div class="posts_area"></div>
+        <img id="#loading" src="assets/images/icons/loading.gif"/>
 
-        $post = new Post($con, $userLoggedIn);
-        $post->loadPostsFriends();
-
-        ?>
     </div>
+    <script>
+        var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+        $(document).ready(function() {
+
+            $('#loading').show();
+
+            //Original ajax request for loading first posts
+            $.ajax({
+                url: "includes/handlers/ajax_load_posts.php",
+                type: "POST",
+                data: "page=1&userLoggedIn=" + userLoggedIn,
+                cache: false,
+
+                success: function(data) {
+                    $('#loading').hide();
+                    $('.posts_area').html(data);
+                }
+            });
+
+            $(window).scroll(function() {
+                var height = $('.posts_area').height(); // Div containing posts
+                var scroll_top = $(this).scrollTop();
+                var page = $('.posts_area').find('.nextPage').val();
+                var noMorePosts = $('posts_area').find('.noMorePosts').val();
+
+                if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false'){
+                    $('#loading').show();
+
+                    var ajaxRequest = $.ajax({
+                        url: "includes/handlers/ajax_load_posts.php",
+                        type: "POST",
+                        data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                        cache: false,
+
+                        success: function(response) {
+                            $('.posts_area').find('.nextPage').remove();  // Removes current next page.
+                            $('.posts_area').find('.noMorePosts').remove();  // Removes current next page.
+
+                            $('#loading').hide();
+                            $('.posts_area').append(response);
+                        }
+                    });
+                }
+            })
+
+        });
+
+
+    </script>
 
     </div>
 </body>
