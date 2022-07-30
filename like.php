d0a38655e8ad4ce527bf33fedcc11959214d281e
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-07-30 01:12:58 +0100

    Finish Newsfeed page / finish styling like buttons

--- like.php
+++ like.php
@@ -1,31 +1,43 @@
 <?php
-    require 'config/config.php';
-    include("includes/classes/User.php");
-    include("includes/classes/Post.php");
-
-    if (isset($_SESSION['username'])) {
-        $userLoggedIn = $_SESSION['username'];
-        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
-        $user = mysqli_fetch_array($user_details_query);
-    }
-    else {
-        header("Location: register.php");
-    }
-
-?> 
-
+        require 'config/config.php';
+        include("includes/classes/User.php");
+        include("includes/classes/Post.php");
+    
+        if (isset($_SESSION['username'])) {
+            $userLoggedIn = $_SESSION['username'];
+            $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+            $user = mysqli_fetch_array($user_details_query);
+        }
+        else {
+            header("Location: register.php");
+        }
+        //Get id of post
+        if(isset($_GET['post_id'])) {
+            $post_id = $_GET['post_id'];
+        }
+?>
 <!DOCTYPE html>
 <head>
     <title></title>
     <link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 <body>
-    <?php
-        //Get id of post
-        if(isset($_GET['post_id'])) {
-            $post_id = $_GET['post_id'];
+    <style type="text/css">
+        *{
+            font-family: Arial, Helvetica, sans-serif;
         }
 
+        body{
+            background-color: #fff;
+        }
+
+        form {
+            position: absolute;
+            top: 0;
+        }
+
+    </style>
+    <?php
         $get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE id ='$post_id'");
         $row = mysqli_fetch_array($get_likes);
         $total_likes = $row['likes'];
@@ -36,9 +48,9 @@
         $total_user_likes = $row['num_likes'];
 
         // Like button
-        if (isset($POST['like_button'])) {
+        if (isset($_POST['like_button'])) {
             $total_likes++;
-            $query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE post_id = '$post_id' ");
+            $query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE id = '$post_id' ");
             $total_user_likes++;
             $user_likes = mysqli_query($con, "UPDATE users SET num_likes = '$total_user_likes' WHERE username = '$user_liked'");
             $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('', '$userLoggedIn', '$post_id')");
@@ -46,7 +58,14 @@
             // Insert Notification
         }
         // Unlike button
+        if (isset($_POST['unlike_button'])) {
+            $total_likes--;
+            $query = mysqli_query($con, "UPDATE posts SET likes = '$total_likes' WHERE id = '$post_id' ");
+            $total_user_likes--;
+            $user_likes = mysqli_query($con, "UPDATE users SET num_likes = '$total_user_likes' WHERE username = '$user_liked'");
+            $insert_user = mysqli_query($con, "DELETE FROM likes WHERE username ='$userLoggedIn' AND post_id = '$post_id'");
 
+        }
 
         //Check for previous likes
         $check_query = mysqli_query($con, "SELECT * FROM likes WHERE username = '$userLoggedIn' AND post_id = '$post_id'");
